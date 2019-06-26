99792714506eda87eeaba01e15d11c1066d6ccf8
Suraj Kumar Shrestha <suraj@united-asian.com>
2019-06-26 12:15:43 +0545

    Update terraform configurations running terraform 0.12upgrade

--- variables.tf
+++ variables.tf
@@ -1,13 +1,14 @@
 variable "cluster_droplet_ids" {
   description = "List of droplet ids"
-  type        = "list"
+  type        = list(string)
 }
 
 variable "cluster_tags" {
   description = "List of droplet tags"
-  type        = "list"
+  type        = list(string)
 }
 
 variable "prefix" {
   description = "Prefix used for the name of the firewall rules"
 }
+
